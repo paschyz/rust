0588409a5eb26c36e7ad81f407116fb67fa0d421
paschyz <paschy2002@gmail.com>
2024-03-02 21:18:55 +0100

    build ci

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+/// This is the entry point of the Rust program.
+///
+/// # Examples
+///
+/// ```
+/// // Calling the main function
+/// fn main() {
+///     println!("Hello, world!");
+/// }
+/// ```
+fn main() {
+    println!("Hello, world!");
+}
